27108ba3c937b9e32154d6497c901ab44cd39316
David Molineus <support@marmalade.de>
2012-11-27 23:26:22 +0100

    add Contao 2.11 support

---
+++ BackendImprovedTheme.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   be_improved_theme
+ * @author    David Molineus <http://www.netzmacht.de>
+ * @license   GNU/LGPL 
+ * @copyright Copyright 2012 David Molineus netzmacht creative 
+ * 
+ */
+
+// fake class for Contao 2.11 backwards compatibility
+require_once TL_ROOT . '/system/modules/be_improved_theme/classes/BackendImprovedTheme.php';
+
+class BackendImprovedTheme extends Netzmacht\BackendImprovedTheme
+{
+}
